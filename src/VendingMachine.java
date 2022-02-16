88ceba772d67bd3f495c6c70c597df26d52ffaa9
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-02-16 15:09:29 -0500

    Added player balance and overpurchase prevention

--- src/VendingMachine.java
+++ src/VendingMachine.java
@@ -15,6 +15,7 @@ public class VendingMachine {
     private static Scanner scan = new Scanner(System.in);
     private static ArrayList<String> item = new ArrayList<String>();
     private static Integer cost = 0;
+    private static int balance;
     private static String reply = "";
 
     private static final HashMap <String,Integer> options = new HashMap<String, Integer>(){{
@@ -27,17 +28,30 @@ public class VendingMachine {
     public static void main(String[] args) {
         System.out.println("Welcome to the " + NAME);
         printInstructions();
+        enterMoney();
         do{
             printMenu();
+            System.out.println("Your current balance is: " + (balance - returnPrice(item)));
             System.out.println("Please enter the food item you would like: ");
-            item.add(scan.next());
+            String choice = scan.nextLine();
+            item.add(choice);
+            if(returnPrice(item) > balance) {
+                System.out.println("You can't afford that!");
+                item.remove(choice);
+            }
             System.out.println("Would you like another item? Yes|No");
-            reply = scan.next();
+            reply = scan.nextLine();
         }while(reply.equals("Yes"));
 
         //prints out the total price of the items the user wants
         System.out.println("The cost of your request is: " + returnPrice(item));
+        System.out.println("Your change will be " + (balance - returnPrice(item)));
 
+    }
+
+    public static void enterMoney() {
+        System.out.println("Please type the number of dollars you would like to enter");
+        balance = Integer.parseInt(scan.nextLine());
     }
 
     //prints instructions for machine
@@ -53,7 +67,7 @@ public class VendingMachine {
     public static int returnPrice(ArrayList <String> objects){
         int c = 0;
         for(int i = 0; i < objects.size(); i++){
-            c = c + options.get(objects.get(i));
+            c = c + options.getOrDefault(objects.get(i),0);
         }
         return c;
     }
